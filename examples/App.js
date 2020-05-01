d24c76ca02c2b1f6b1ca236aca7deba24a14429a
DragonCaster96 <holmesart3@aol.com>
2020-05-01 12:06:43 -0500

    Ok
    asdf

--- examples/App.js
+++ examples/App.js
@@ -88,76 +88,8 @@ class Example extends React.Component {
       <div className="app">
         <div className="jumbotron">
           <div className="container">
-            <h1>
-              Big Calendar <i className="fa fa-calendar" />
-            </h1>
-            <p>such enterprise, very business.</p>
-            <p>
-              <a href="#intro">
-                <i className="fa fa-play" /> Getting started
-              </a>
-              {' | '}
-              <a href="#api">
-                <i className="fa fa-book" /> API documentation
-              </a>
-              {' | '}
-              <a
-                target="_blank"
-                href="https://github.com/intljusticemission/react-big-calendar"
-              >
-                <i className="fa fa-github" /> github
-              </a>
-            </p>
+          
           </div>
-        </div>
-        <div className="examples">
-          <Card className="examples--header">
-            <Layout
-              align="center"
-              justify="space-between"
-              style={{ marginBottom: 15 }}
-            >
-              <div className="examples--view-source">
-                <a target="_blank" href={demoRoot + '/' + selected + '.js'}>
-                  <strong>
-                    <i className="fa fa-code" />
-                    {' View example source code'}
-                  </strong>
-                </a>
-              </div>
-              <Dropdown
-                pullRight
-                id="examples-dropdown"
-                className="examples--dropdown"
-              >
-                <Dropdown.Toggle bsStyle="link" className="dropdown--toggle ">
-                  {EXAMPLES[selected]}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  {Object.entries(EXAMPLES).map(([key, title]) => (
-                    <MenuItem
-                      active={this.state.selected === key}
-                      key={key}
-                      href={`#${key}`}
-                      onClick={() => this.select(key)}
-                    >
-                      {title}
-                    </MenuItem>
-                  ))}
-                </Dropdown.Menu>
-              </Dropdown>
-            </Layout>
-            <ExampleControlSlot.Outlet />
-          </Card>
-          <div className="example">
-            <Current localizer={globalizeLocalizer} />
-          </div>
-        </div>
-        <div className="docs">
-          <div className="contain section">
-            <Intro />
-          </div>
-          <Api className="contain section" />
         </div>
       </div>
     )
